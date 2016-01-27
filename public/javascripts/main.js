7209f40018523635fd6fcc64631b7d3ed32dcbbe
Zack Winchell <zackwinchell@gmail.com>
2016-01-27 15:24:37 -0800

    new products can now be added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,40 @@
 "use strict";
 
-console.log("javascript loaded");
+var $productList;
 
 $(document).ready(init);
 
 function init(){
+  $productList = $("#productList");
   $('#addProduct').submit(addProduct);
-  $("#productList").on("click", "li", productClick);
+  $productList.on("click", "li", productClick);
 }
 
 function addProduct(e){
   e.preventDefault();
-  console.log("name", $('#itemName').val());
-  console.log("cost", "$" + Number($('#itemCost').val()).toFixed(2));
-  console.log("count", $('#itemCount').val());
+  $.post("/product", {
+    name: $('#itemName').val(),
+    desc: $('#itemDesc').val(),
+    cost: Number($('#itemCost').val()).toFixed(2),
+    count: $('#itemCount').val()})
+  .success(function(data){
+    console.log("success:", data);
+    var newItem = $("<li>").data("_id", data._id);
+    var newItemContents = $('<div>').addClass("row");
+    newItemContents.append($('<p>').text(data.name).addClass("col-xs-3"));
+    newItemContents.append($('<p>').text(data.desc).addClass("col-xs-3"));
+    newItemContents.append($('<p>').text(data.cost).addClass("col-xs-3"));
+    newItemContents.append($('<p>').text(data.count).addClass("col-xs-3"));
+    newItem.append(newItemContents);
+    $productList.append(newItem);
+    // console.log("_id:", data._id);
+  })
+  .fail(function(err){
+    console.log("err:",err);
+  })
 }
 
 function productClick(e){
   e.stopPropagation();
-  console.log(Date.now());
+  console.log("_id:", $(this).closest('li').data("_id"));
 }
--- routes/product.js
+++ routes/product.js
@@ -2,10 +2,41 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/products");
+
+var Item = mongoose.model("Item", {
+  name: String,
+  desc: String,
+  cost: Number,
+  count: Number
+});
+
+router.post("/", function(req, res, next){
+  console.log("req.body:", req.body);
+
+  var newItem = new Item({
+    name: req.body.name,
+    desc: req.body.desc,
+    cost: req.body.cost,
+    count: req.body.count
+  })
+
+  newItem.save(function (err, savedItem) {
+    if (err) return console.error(err);
+    res.send(savedItem); //also send ID to be attatched to the DOM as data("_id", id)
+  });
+
+})
+
 /* GET home page. */
 router.get('/:productId', function(req, res, next) {
   //do stuff
   res.render('product', { id: req.body });
+});
+
+router.put("/:productId", function(req, res, next){
+
+  res.send("Put received:", [req.body, {productId}]);
 });
 
 module.exports = router;
